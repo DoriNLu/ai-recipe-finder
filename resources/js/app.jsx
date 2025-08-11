ee5a0ebadafb5c28a7d1507653d180d932c77b35
DoriNLu <120054639+DoriNLu@users.noreply.github.com>
2025-08-11 16:29:29 +0300

    Update app.jsx
    mesage

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import 'bootstrap/dist/css/bootstrap.min.css'; // dacă folosești Bootstrap
-import 'bootstrap-icons/font/bootstrap-icons.css'; // pentru iconițe
-import './components/SearchForm.css'; // sau orice alt CSS custom
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap-icons/font/bootstrap-icons.css'; 
+import './components/SearchForm.css'; 
 
 const queryClient = new QueryClient();
 
